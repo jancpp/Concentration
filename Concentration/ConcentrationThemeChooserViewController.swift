5a604752a9f0f8175bda435568aaf53535473371
Jan Polzer <janman74@gmail.com>
2018-06-22 12:35:42 -0500

    Made Theme chooser to be the first screen, split view works on both small and big screens

--- Concentration/ConcentrationThemeChooserViewController.swift
+++ Concentration/ConcentrationThemeChooserViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ConcentrationThemeChooserViewController: UIViewController {
+// delegate for split view controller
+class ConcentrationThemeChooserViewController: UIViewController, UISplitViewControllerDelegate {
 
     let themes = [ // bad design, what if we add multiple languages?
         "Helloween":"👻🎃🍎🙀🧟‍♂️👿🦇🍭🧛🏼‍♂️🧛‍♀️👹",
@@ -16,13 +17,57 @@ class ConcentrationThemeChooserViewController: UIViewController {
         "Sports":"🏈🎾🏀🎱⚽️⚾️🏓🏒🏐🥊🥌⛳️",
     ]
     
-//     MARK: - Navigation
-
+    override func awakeFromNib() {
+        splitViewController?.delegate = self
+    }
+    
+    // colapses detail on top of master, on small screens, nil theme = don't colapse = true
+    func splitViewController(
+        _ splitViewController: UISplitViewController,
+        collapseSecondary secondaryViewController: UIViewController, // detail
+        onto primaryViewController: UIViewController  // master
+        ) -> Bool {
+        if let cvc = secondaryViewController as? ConcentrationViewController {
+            if cvc.theme == nil {
+                return true
+            }
+        }
+        return false // means: I didn't colapse it, you should do it
+    }
+    
+    @IBAction func changeTheme(_ sender: Any) {
+        // split view for bigger screens
+        if let cvc = splitViewDetailConcentratoinViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+        }
+        // this will hold view in heap, so we can change theme on small screens
+        else if let cvc = lastSequedToConcentrationViewController {
+            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+                cvc.theme = theme
+            }
+            navigationController?.pushViewController(cvc, animated: true)
+        } else {
+            // seque from code
+            performSegue(withIdentifier: "Choose Theme", sender: sender)
+        }
+    }
+    
+    private var splitViewDetailConcentratoinViewController: ConcentrationViewController? {
+        return splitViewController?.viewControllers.last as? ConcentrationViewController
+    }
+    
+    // MARK: - Navigation
+    
+    private var lastSequedToConcentrationViewController: ConcentrationViewController?
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "Choose Theme" {
             if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
                 if let cvc = segue.destination as? ConcentrationViewController {
                     cvc.theme = theme
+                    lastSequedToConcentrationViewController = cvc
                 }
             }
         }
